08156434b51afe44d9c9662d01407699cedc6880
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-02-10 22:39:45 +0530

    ready

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const app=express();
+
+const PORT=process.env.PORT || 8080
+
+app.get('/',(req,res) =>{
+    res.send("Crud Application");
+})
+
+app.listen(PORT, () =>{
+    console.log('Server Running on ',{PORT});
+});

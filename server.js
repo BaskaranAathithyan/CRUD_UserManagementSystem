ee6edbc2331094d4a062acbc2e97e5e920aaaf1d
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-02-12 21:48:26 +0530

    views

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.set("view engine","ejs")
 //app.set("views",path.resolve(__dirname, "views/ejs"))
 
 //load assets
-app.use('/css'.express.static(path.resolve(__dirname,"assets/css")))
-app.use('/img'.express.static(path.resolve(__dirname,"assets/img")))
-app.use('/js'.express.static(path.resolve(__dirname,"assets/js")))
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
 
 app.get('/',(req,res) =>{
-    res.send("Crud Application");
+    res.render('index');
 })
 
 app.listen(PORT, () =>{
